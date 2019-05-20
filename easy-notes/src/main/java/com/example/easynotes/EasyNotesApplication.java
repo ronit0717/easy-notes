9023e710ef788926b485e774377164c5a76cf99e
Ronit Chattopadhyay <ronit0717@gmail.com>
2019-05-20 08:29:55 +0530

    RC Commit 2

--- easy-notes/src/main/java/com/example/easynotes/EasyNotesApplication.java
+++ easy-notes/src/main/java/com/example/easynotes/EasyNotesApplication.java
@@ -3,6 +3,8 @@ package com.example.easynotes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//Entry Point
 @SpringBootApplication
 public class EasyNotesApplication {
 
